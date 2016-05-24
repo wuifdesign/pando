abb54d6683d27792d6fa61068eb0cf398d3d74e0
Michael Wohlfahrter <m.wohlfahrter@ixolit.com>
2016-05-24 16:00:14 +0200

    config gulp

--- gulp-js-paths-vendor.js
+++ gulp-js-paths-vendor.js
@@ -4,14 +4,15 @@ module.exports = [
     'components/anijs/dist/anijs.js',
     'components/bootstrap-sass/assets/javascripts/bootstrap.js',
     'components/bootstrap3-dialog/dist/js/bootstrap-dialog.js',
-    'components/magnific-popup/dist/jquery.magnific-popup.js',
     'components/slick-carousel/slick/slick.js',
     'components/toastr/toastr.js',
 
-    'js/elements/pando/pandoEqualizer.js',
-    'js/elements/pando/pandoLayout.js',
-    'js/elements/pando/pandoParallax.js',
-    'js/elements/pando/pandoShake.js',
-    'js/elements/magnific-popup-override.js',
-    'js/main.js'
+    'components/magnific-popup/dist/jquery.magnific-popup.js',
+    'js/pando/magnific-popup-override.js',
+
+    'js/pando/pando-equalizer.js',
+    'js/pando/pando-layout.js',
+    'js/pando/pando-parallax.js',
+    'js/pando/pando-shake.js',
+    'js/pando/pando.js'
 ];
--- resources/gulp-tasks/js-vendor.js
+++ resources/gulp-tasks/js-vendor.js
@@ -1,13 +1,13 @@
 module.exports = function (gulp, plugins, config) {
     return function () {
-        var crystalJsPaths  = require('../../gulp-js-paths-custom.js');
+        var crystalJsPaths  = require('../../gulp-js-paths-vendor.js');
         var crystalPaths = [];
         crystalJsPaths.map(function(path) {
             crystalPaths.push(config.srcPath + '/' + path);
         });
         return gulp.src(crystalPaths)
             .pipe(plugins.sourcemaps.init())
-            .pipe(plugins.concat('main.min.js'))
+            .pipe(plugins.concat('vendor.min.js'))
             .pipe(plugins.uglify())
             .pipe(plugins.sourcemaps.write('./'))
             .pipe(gulp.dest(config.publicPath + '/js'));
