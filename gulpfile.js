1f1e5b4ada83cbbf226e1da8eb0c9399d1c0b580
Michael Wohlfahrter <m.wohlfahrter@ixolit.com>
2016-05-25 11:17:49 +0200

    added sprites

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,8 @@ var config = {
     publicPath: './public/assets' //change to your target path
 };
 
-function getTask(task) {
-    return require(config.srcPath + '/../gulp-tasks/' + task)(gulp, plugins, config);
+function getTask(task, extra) {
+    return require(config.srcPath + '/../gulp-tasks/' + task)(gulp, plugins, config, extra);
 }
 
 gulp.task('copy-files', getTask('copy-files'));
@@ -19,4 +19,11 @@ gulp.task('js', getTask('js'));
 gulp.task('js-vendor', getTask('js-vendor'));
 gulp.task('watch', getTask('watch'));
 
-gulp.task('default', ['copy-files', 'sass', 'js']);
+//Generates Sprite images and adds the image to the "sprites_path" and the SCSS-files to "sass/custom/sprites"
+gulp.task('sprite', getTask('sprite', {
+    enable_retina_sprites: false, //Enable retina sprites which are defined as "filename@2x.png" and have to be exactly double the size of the normal image
+    sprites_path: config.publicPath + '/img/sprites/', //Path to the spite images
+    sprites: [ /* 'base' */ ] //add folder names of the sprites. (if folder is "../img/sprites/base" add "base" to the array
+}));
+
+gulp.task('default', ['copy-files', 'sass', 'js-vendor', 'js']);
