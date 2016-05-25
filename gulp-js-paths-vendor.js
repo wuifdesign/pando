154ae755a8dbfaa7522fdb08414563a65004d7e2
Wuif <5678318+wuifdesign@users.noreply.github.com>
2016-05-25 17:36:21 +0200

    fixes

--- gulp-js-paths-vendor.js
+++ gulp-js-paths-vendor.js
@@ -2,6 +2,7 @@ module.exports = [
     'components/jquery/dist/jquery.js',
 
     'components/anijs/dist/anijs.js',
+    'components/anijs/dist/helpers/scrollreveal/anijs-helper-scrollreveal.js',
     'components/bootstrap-sass/assets/javascripts/bootstrap.js',
     'components/bootstrap3-dialog/dist/js/bootstrap-dialog.js',
     'components/slick-carousel/slick/slick.js',
