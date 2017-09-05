4d97be0c1578cccb0bad57e96447a2fdf57490a9
Wuif <5678318+wuifdesign@users.noreply.github.com>
2017-09-05 17:13:41 +0200

    cleanup docs

--- gulp-js-paths-vendor.js
+++ gulp-js-paths-vendor.js
@@ -6,6 +6,8 @@ module.exports = [
     '../../node_modules/slick-carousel/slick/slick.js',
     '../../node_modules/toastr/toastr.js',
     '../../node_modules/svg4everybody/dist/svg4everybody.js',
+    '../../node_modules/AniJS/dist/anijs.js',
+    '../../node_modules/AniJS/dist/helpers/scrollreveal/anijs-helper-scrollreveal.js',
 
     '../../node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     'js/pando/magnific-popup-override.js'
