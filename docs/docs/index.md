f7b7a65835d314856dc0a235a54f5292f945bc33
Michael Wohlfahrter <m.wohlfahrter@gmail.com>
2017-09-05 16:55:05 +0200

    updated docs/sourcefiles

--- docs/docs/index.md
+++ docs/docs/index.md
@@ -6,13 +6,7 @@ excerpt: Special Components added to Bootstrap
 ---
 
 {% include components/sprites.html %}
-{% include components/grid.html %}
-{% include components/margin.html %}
 {% include components/alerts.html %}
 {% include components/buttons.html %}
-{% include components/nav.html %}
-{% include components/badge.html %}
 {% include components/form.html %}
-{% include components/widget.html %}
-{% include components/progress.html %}
 {% include components/random.html %}
--- resources/gulp-tasks/copy-files.js
+++ resources/gulp-tasks/copy-files.js
@@ -1,9 +1,10 @@
 module.exports = function (gulp, plugins, config) {
     return function () {
-        gulp.src(config.src_path + '/components/**/*.{png,jpg,gif}', { base: config.src_path + '/components' }).pipe(gulp.dest(config.public_path + '/img/vendor'));
-
-        gulp.src(config.src_path + '/components/bootstrap-sass/**/*.{eot,svg,ttf,woff,woff2}', { base: config.src_path + '/components' }).pipe(gulp.dest(config.public_path + '/fonts'));
-        gulp.src(config.src_path + '/components/font-awesome/**/*.{eot,svg,ttf,woff,woff2}', { base: config.src_path + '/components' }).pipe(gulp.dest(config.public_path + '/fonts'));
-        gulp.src(config.src_path + '/components/slick-carousel/**/*.{eot,svg,ttf,woff,woff2}', { base: config.src_path + '/components' }).pipe(gulp.dest(config.public_path + '/fonts'));
+        ['font-awesome', 'slick-carousel'].forEach(function (item) {
+            console.log(item);
+            console.log(config.src_path + '/../../node_modules/' + item + '/**/*.{png,jpg,gif}');
+            gulp.src(config.src_path + '/../../node_modules/' + item + '/**/*.{png,jpg,gif}', { base: config.src_path + '/../../node_modules' }).pipe(gulp.dest(config.public_path + '/img/vendor'));
+            gulp.src(config.src_path + '/../../node_modules/' + item + '/**/*.{eot,svg,ttf,woff,woff2}', { base: config.src_path + '/../../node_modules' }).pipe(gulp.dest(config.public_path + '/fonts'));
+        });
     };
 };
