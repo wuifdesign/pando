de2c03de6c5440cfdb0b41b96e79423352c68e4a
Wuif <5678318+wuifdesign@users.noreply.github.com>
2017-09-06 13:04:48 +0200

    cleanup

--- resources/assets/js/pando/pando-layout.js
+++ resources/assets/js/pando/pando-layout.js
@@ -3,12 +3,10 @@
 
     if(typeof $ !== 'undefined') {
         var body;
-        var fixedTop;
         var fixedTopAuto;
 
         $(document).ready(function() {
             body = $('body');
-            fixedTop = $('.navbar.fixed-top');
             fixedTopAuto = $('.navbar.fixed-top.auto-padding');
             $('.btn-scroll-top').pandoOnClickScrollTop();
             fixedCheck();
@@ -23,9 +21,8 @@
         });
 
         var fixedCheck = function() {
-            if(typeof body === 'undefined') { return; }
-            if(body.hasClass('footer-fixed') || body.hasClass('footer-bottom')) {
-                $('.footer-push').css('height', $('.footer').outerHeight(true));
+            if(typeof body === 'undefined') {
+                return;
             }
             if(fixedTopAuto.length > 0) {
                 body.css('padding-top', fixedTopAuto.outerHeight(true));
@@ -37,19 +34,10 @@
                 return;
             }
             var scrollTop = $(window).scrollTop();
-
             if(scrollTop > 5) {
                 body.addClass('is-scrolled');
             } else {
                 body.removeClass('is-scrolled');
-            }
-
-            if(fixedTop.length > 0) {
-                if(scrollTop > 5) {
-                    fixedTop.addClass('is-scrolled');
-                } else {
-                    fixedTop.removeClass('is-scrolled');
-                }
             }
         };
 
@@ -60,8 +48,12 @@
         };
 
         $.fn.pandoScrollTo = function(offset, speed_divider) {
-            if(typeof offset === 'undefined') {offset = 0; }
-            if(typeof speed_divider === 'undefined') { speed_divider = 7; }
+            if(typeof offset === 'undefined') {
+                offset = 0;
+            }
+            if(typeof speed_divider === 'undefined') {
+                speed_divider = 7;
+            }
             var window_top = $(window).scrollTop();
             var element_top = $($(this).first()).offset().top - offset;
             var speed = (element_top - window_top) / speed_divider;
@@ -70,6 +62,6 @@
             }
             $('html,body').animate({scrollTop: element_top}, speed);
         };
-    } 
+    }
 
 }(jQuery));
