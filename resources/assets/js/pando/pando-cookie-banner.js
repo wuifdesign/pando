5d9e361bd650b89db1145c570b323ad33fd1e810
Wuif <5678318+wuifdesign@users.noreply.github.com>
2018-07-20 10:46:08 +0200

    update for node 10

--- resources/assets/js/pando/pando-cookie-banner.js
+++ resources/assets/js/pando/pando-cookie-banner.js
@@ -14,6 +14,11 @@
     if(window.getCookie('cookies_accepted') !== 'yes') {
       banner.show();
       banner_spacer.show();
+      try {
+        setTimeout(function() {
+          $.pandoFooterPush();
+        }, 10);
+      } catch(error) {}
     }
 
     close_button.on('click', function() {
