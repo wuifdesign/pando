ae043f27dda9d577ab5768608c14e50cb5ffe76d
Wuif <5678318+wuifdesign@users.noreply.github.com>
2018-11-28 14:36:28 +0100

    updated to gulp4

--- gulp-js-paths-vendor.js
+++ gulp-js-paths-vendor.js
@@ -1,21 +1,21 @@
 module.exports = [
-  'components/jquery/dist/jquery.js',
-  'components/popper.js/dist/umd/popper.js',
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/popper.js/dist/umd/popper.js',
 
-  'components/bootstrap/dist/js/bootstrap.js',
-  'components/slick-carousel/slick/slick.js',
-  'components/toastr/toastr.js',
-  'components/svg4everybody/dist/svg4everybody.js',
-  'components/AniJS/dist/anijs.js',
-  'components/AniJS/dist/helpers/scrollreveal/anijs-helper-scrollreveal.js',
+  'node_modules/bootstrap/dist/js/bootstrap.js',
+  'node_modules/slick-carousel/slick/slick.js',
+  'node_modules/toastr/toastr.js',
+  'node_modules/svg4everybody/dist/svg4everybody.js',
+  'node_modules/AniJS/dist/anijs.js',
+  'node_modules/AniJS/dist/helpers/scrollreveal/anijs-helper-scrollreveal.js',
 
-  'components/jquery-validation/dist/jquery.validate.js',
-  //'components/jquery-validation/dist/localization/messages_de.js',
+  'node_modules/jquery-validation/dist/jquery.validate.js',
+  //'node_modules/jquery-validation/dist/localization/messages_de.js',
 
-  'components/photoswipe/dist/photoswipe.js',
-  'components/photoswipe/dist/photoswipe-ui-default.js',
+  'node_modules/photoswipe/dist/photoswipe.js',
+  'node_modules/photoswipe/dist/photoswipe-ui-default.js',
   'js/pando/photoswipe-override.js',
 
-  'components/magnific-popup/dist/jquery.magnific-popup.js',
+  'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
   'js/pando/magnific-popup-override.js'
 ];
--- resources/gulp-tasks/copy-files.js
+++ resources/gulp-tasks/copy-files.js
@@ -1,23 +1,24 @@
 module.exports = function(gulp, plugins, config) {
-  return function() {
-    gulp.src(config.src_path + '/components/photoswipe/dist/**/*.{png,svg,jpg,gif}', {base: config.src_path + '/components'})
+  return function(done) {
+    gulp.src(config.src_path + '/node_modules/photoswipe/dist/**/*.{png,svg,jpg,gif}', {base: config.src_path + '/node_modules'})
       .pipe(plugins.rename(function(path) {
         path.dirname = 'photoswipe';
       }))
       .pipe(gulp.dest(config.public_path + '/img/vendor'));
 
     [{src: 'slick-carousel', dest: 'slick-carousel'}, {src: '@fortawesome/fontawesome-free/webfonts', dest: 'fontawesome'}].forEach(function(item) {
-      gulp.src(config.src_path + '/components/' + item.src + '/**/*.{png,jpg,gif}', {base: config.src_path + '/components'})
+      gulp.src(config.src_path + '/node_modules/' + item.src + '/**/*.{png,jpg,gif}', {base: config.src_path + '/node_modules'})
         .pipe(plugins.rename(function(path) {
           path.dirname = item.dest;
         }))
         .pipe(gulp.dest(config.public_path + '/img/vendor'));
 
-      gulp.src(config.src_path + '/components/' + item.src + '/**/*.{eot,svg,ttf,woff,woff2}', {base: config.src_path + '/components'})
+      gulp.src(config.src_path + '/node_modules/' + item.src + '/**/*.{eot,svg,ttf,woff,woff2}', {base: config.src_path + '/node_modules'})
         .pipe(plugins.rename(function(path) {
           path.dirname = item.dest;
         }))
         .pipe(gulp.dest(config.public_path + '/fonts/'));
     });
+    done();
   };
 };
